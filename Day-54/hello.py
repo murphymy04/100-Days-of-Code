0646288360b296c6d9452a3b5f15e716f7622d9f
murphymy04 <137734306+murphymy04@users.noreply.github.com>
2023-07-17 16:11:19 -0500

    Day 54

--- Day-54/hello.py
+++ Day-54/hello.py
@@ -1,7 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+# def decorator_function(function):
+#     def wrapper_function():
+#         function()
+#     return wrapper_function
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
+
+@app.route("/bye")
+def say_bye():
+    return "Bye"
+
+if __name__ == "__main__":
+    app.run()
